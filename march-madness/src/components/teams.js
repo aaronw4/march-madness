dac272a124098178e1d61657f059170ae66000e5
Aaron Williams <aaronrw4@yahoo.com>
2020-02-23 06:43:55 -0800

    Created function to order list by team name

--- march-madness/src/components/teams.js
+++ march-madness/src/components/teams.js
@@ -3,6 +3,7 @@ import axiosWithAuth from './axiosWithAuth';
 
 const Teams = () => {
     const [teams, setTeams] = useState([]);
+    const [list, setList] = useState([]);
 
     useEffect(() => {
         function fetchData() {
@@ -14,14 +15,40 @@ const Teams = () => {
                 })
                 .catch(err => console.log(err))
         }
-        fetchData();
+        fetchData();        
     },[]);
+
+    useEffect(() => {
+        function newList() {
+            let list = teams.map(team => ({
+                id: team.team.id,
+                name: team.team.name,
+                wins: team.games.win.total,
+                loses: team.games.lose.total
+            }));
+    
+            let orderedList = list.sort(function(a,b) {
+                var name1 = a.name;
+                var name2 = b.name;
+                if (name1 < name2) {
+                    return -1;
+                }
+                if (name1 > name2) {
+                    return +1;
+                }
+                return 0;
+            });
+
+            setList(orderedList);
+        } 
+        newList();
+    },[teams]);
 
     return (
         <div>
-            {teams.map(team => (
-                <div key={team.team.id}>
-                    <p>{team.team.name}({team.games.win.total}-{team.games.lose.total})</p>
+            {list.map(team => (
+                <div key={team.id}>
+                    <p>{team.name}({team.wins}-{team.loses})</p>
                 </div>
             ))}
         </div>
