ad751cf22368618f6a23798113f69a4e3bcb1ceb
Aaron Williams <aaronrw4@yahoo.com>
2020-02-29 16:08:40 -0800

    update package.json

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const cors = require('cors')
 
 const teamsRouter = require('./teams/teamsRouter');
 
 const server = express();
 
 server.use(express.json());
-server.use(cors());
 server.use('/teams', teamsRouter);
 
 module.exports = server;
