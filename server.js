eed12de3d695758724c797b63a9db0894dc70d96
Aaron Williams <aaronrw4@yahoo.com>
2020-02-29 16:19:21 -0800

    Add cors to server

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
-
+const cors = require('cors');
 const teamsRouter = require('./teams/teamsRouter');
 
 const server = express();
 
 server.use(express.json());
+server.use(cors());
+
 server.use('/teams', teamsRouter);
 
 module.exports = server;
