90b529dbc419d5296f3840d5dec9523f8891996b
Aaron Williams <aaronrw4@yahoo.com>
2020-03-06 22:14:25 -0800

    Created matchupsModel.js

---
+++ team-matchups/matchupsModel.js
@@ -0,0 +1,22 @@
+const db = require('../data/dbConfig');
+
+function add(teams) {
+    return db('matchups')
+        .insert(teams, 'id')
+}
+
+function find() {
+    return db('matchups')
+}
+
+function remove(id) {
+    return db('mathups')
+        .where({id})
+        .delete()
+}
+
+module.exports = {
+    add,
+    find,
+    delete
+}
