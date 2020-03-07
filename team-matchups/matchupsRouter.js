1e6a88272973947116c8832866a91f35361170e7
Aaron Williams <aaronrw4@yahoo.com>
2020-03-06 22:20:56 -0800

    Created matchupsRouter.js

---
+++ team-matchups/matchupsRouter.js
@@ -0,0 +1,39 @@
+const matchup = require('./matchupsModel');
+
+const router = require('express').Router();
+
+router.post('/', (req, res) => {
+    let teams = req.body;
+
+    matchup.add(teams)
+        .then(info => {
+            res.status(200).json(info)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+});
+
+router.get('/', (req, res) => {
+    matchup.find()
+        .then(info => {
+            res.status(200).json(info)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+});
+
+router.delete('/', (req, res) => {
+    const {id} = req.params;
+
+    matchup.remove(id)
+        .then(matchup => {
+            res.status(200).json({removed: matchup})
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+
+module.exports = router;
