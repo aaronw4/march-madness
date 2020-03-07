a2b16e876f9f8b77a7dcabd93dea0f97cf0944c9
Aaron Williams <aaronrw4@yahoo.com>
2020-03-06 21:44:57 -0800

    Created /matchups route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const teamsRouter = require('./teams/teamsRouter');
+const matchupsRouter = require('./team-matchups/matchupsRouter');
 
 const server = express();
 
@@ -8,5 +9,6 @@ server.use(express.json());
 server.use(cors());
 
 server.use('/teams', teamsRouter);
+server.use('/matchups', matchupsRouter);
 
 module.exports = server;
