dbb0ef287724238454e1e2e9269bbf9ca51d7375
Aaron Williams <aaronrw4@yahoo.com>
2020-03-08 16:06:01 -0700

    Added producation to knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -14,5 +14,12 @@ module.exports = {
     seeds: {
       directory: './data/seeds'
     }
-  }
+  },
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'postgres://aoxnetcagsauno:81b3b999ba2558ee39a8b56a5919e9decc7178d69fb7ffc3d2f1920238a802ad@ec2-184-72-235-80.compute-1.amazonaws.com:5432/de2n227amjdm5g',
+      user:     'aoxnetcagsauno',
+      password: '81b3b999ba2558ee39a8b56a5919e9decc7178d69fb7ffc3d2f1920238a802ad'
+    }
 };
