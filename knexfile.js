1d36110adeb290d868929c2563a00fa8734b8e24
Aaron Williams <aaronrw4@yahoo.com>
2020-03-08 16:28:17 -0700

    Added migrations and seeds path to production

--- knexfile.js
+++ knexfile.js
@@ -18,9 +18,15 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      database: 'postgres://aoxnetcagsauno:81b3b999ba2558ee39a8b56a5919e9decc7178d69fb7ffc3d2f1920238a802ad@ec2-184-72-235-80.compute-1.amazonaws.com:5432/de2n227amjdm5g',
+      database: process.env.DATABASE_URL,
       user:     'aoxnetcagsauno',
       password: '81b3b999ba2558ee39a8b56a5919e9decc7178d69fb7ffc3d2f1920238a802ad'
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
     }
   }
 }
