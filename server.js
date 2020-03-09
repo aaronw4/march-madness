d4b5789e8dd014bc77ea501fc61ba32f38534a70
Aaron Williams <aaronrw4@yahoo.com>
2020-03-09 15:13:02 -0700

    Added options to cors

--- server.js
+++ server.js
@@ -3,10 +3,16 @@ const cors = require('cors');
 const teamsRouter = require('./teams/teamsRouter');
 const matchupsRouter = require('./team-matchups/matchupsRouter');
 
+const corsOptions = {
+    origin: true,
+    credentials: true,
+    methods: ['GET', 'POST', 'DELETE']
+};
+
 const server = express();
 
 server.use(express.json());
-server.use(cors());
+server.use(cors(corsOptions));
 
 server.use('/teams', teamsRouter);
 server.use('/matchups', matchupsRouter);
