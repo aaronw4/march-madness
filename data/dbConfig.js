5852eaa368caf74c63bcd0b796b8df722cfa08f7
Aaron Williams <aaronrw4@yahoo.com>
2020-03-09 09:33:54 -0700

    Moved files to dbConfig

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -2,6 +2,23 @@ const knex = require('knex');
 
 const config = require('../knexfile');
 
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+
 const environment = process.env.DB_ENV || 'development';
 
 module.exports = knex(config[environment]);
