4d2b08739f485c3d5e207345d905f8df1baac47c
Aaron Williams <aaronrw4@yahoo.com>
2020-03-09 17:11:35 -0700

    Added Client

--- knexfile.js
+++ knexfile.js
@@ -2,17 +2,15 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './data/teams.db3'
-    },
-    useNullAsDefault: true,
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './data/migrations'
     },
     seeds: {
       directory: './data/seeds'
-    }
+    },
+    useNullAsDefault: true
   },
   production: {
     client: 'pg',
--- teams/teamsModel.js
+++ teams/teamsModel.js
@@ -1,22 +1,23 @@
-const db = require('../data/dbConfig');
+const {Client} = require('pg');
+
+const client = new Client({
+    user: 'aoxnetcagsauno',
+    password: '81b3b999ba2558ee39a8b56a5919e9decc7178d69fb7ffc3d2f1920238a802ad',
+    host: 'ec2-184-72-235-80.compute-1.amazonaws.com',
+    database: 'de2n227amjdm5g',
+    port: 5432
+});
 
 function add(team) {
-    return db('teams')
-        .insert(team, 'id')
-}
-
-function find() {
-    return db('teams')
-}
-
-function remove(id) {
-    return db('teams')
-        .where({id})
-        .delete()
+    client.connect()
+        .then(() => client.query(`INSERT INTO teams VALUES(${team})`))
+        .then(() => )
+        .catch(err => console.log(err))
+        .finally(() => client.end())
 }
 
 module.exports = {
     add,
-    find,
-    remove
+    // find,
+    // remove
 }
